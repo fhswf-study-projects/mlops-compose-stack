621a3fcf03ba2e1335586a3a969abd68b5445f21
VitaliKStud <vitali9394@yahoo.de>
2025-03-18 19:16:01 +0100

    fix: ipynb-to-html

--- docs/convert_ipynb_to_html.py
+++ docs/convert_ipynb_to_html.py
@@ -4,14 +4,14 @@ import logging
 
 try:
     exporter = nbconvert.HTMLExporter()
-    logging.warning(os.listdir())
-    all_notebooks = [book for book in os.listdir('_static\\notebooks') if ".ipynb" in book]
+    logging.warning(os.listdir("_static"))
+    all_notebooks = [book for book in os.listdir('_static/notebooks') if ".ipynb" in book]
     for notebook in all_notebooks:
 
-        body, _ = exporter.from_filename(f"_static\\notebooks\\{notebook}")
+        body, _ = exporter.from_filename(f"_static/notebooks/{notebook}")
 
         new_nb_name = notebook.replace(".ipynb", ".html")
-        with open(f"_static\\notebooks\\{new_nb_name}", "w", encoding="utf-8") as f:
+        with open(f"_static/notebooks/{new_nb_name}", "w", encoding="utf-8") as f:
             f.write(body)
 except Exception as e:
     logging.error(f"FAILED TO DOCUMENT NOTEBOOKS: {e}")
